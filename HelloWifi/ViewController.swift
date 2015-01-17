46abf9cfaa8eb2bc9c6ceeef521ffb38180b25c7
Donald Burr <dburr@borg-cube.com>
2015-01-17 15:35:49 -0800

    Initial Commit

---
+++ HelloWifi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWifi
+//
+//  Created by Donald Burr on 1/17/15.
+//  Copyright (c) 2015 Donald Burr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
